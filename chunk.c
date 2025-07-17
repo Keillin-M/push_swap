5b802aa06914bb55b6cbb6b03b8c52a126698d1f
Keillin Maeda <kmaeda@c4a6c4.42berlin.de>
2025-07-17 17:55:30 +0200

    Phase 4 - part1

--- chunk.c
+++ chunk.c
@@ -12,39 +12,115 @@
 
 #include "push_swap.h"
 
-int	chunk_size(t_data *data)
+int	chunk_count(t_data *data)
 {
 	int	target;
 
 	target = 0;
 	if (data->size <= 10)
 		target = 2;
-	if (data->size <= 100)
+	else if (data->size <= 100)
 		target = 5;
-	if (data->size > 100 && data->size <= 250)
+	else if (data->size > 100 && data->size <= 250)
 		target = 10;
-	if (data->size > 250)
+	else if (data->size > 250)
 		target = 20;
 	return (target);
 }
 
-void	position(t_list **a, t_data *data)
+int	position(t_list **a, t_data *data)
 {
-	int	count;
-	int	target;
+	int	i;
+	t_list	*first;
+	t_list	*last;
+
+	i = 0;
+	first = *a;
+	last = first;
+	data->top_pos = -1;
+	while (i < data->new_size)
+	{
+		if (last->index <= data->chunks)
+			data->tail_pos = i;
+		i++;
+		last = last->next;
+	}
+	while (first->index <= data->chunks)
+	{
+		data->top_pos++;
+		first = first->next;
+	}
+	if (data->top_pos < data->tail_pos)
+		return (1);
+	else
+		return (0);
+	return (-1);
+}
+
+int	chunk_part(t_list **a, t_list **b, t_data *data)
+{
+	int	pos;
+
+	pos = position(a, data);
+	if (pos < 0)
+		return (1);
+	else if (pos == 1)
+	{
+		while (data->top_pos > 0)
+		{
+			ra(a);
+			data->top_pos--;
+		}
+		pb(a, b);
+	}
+	else if (pos == 0)
+	{
+		while (data->tail_pos > 0)
+		{
+			rra(a);
+			data->tail_pos--;
+		}
+		pb(a, b);
+	}
+	return (0);
+}
+
+void	chunk_sort(t_list **a, t_list **b, t_data *data)
+{
 	t_list	*temp;
 
-	count = 0;
 	temp = *a;
-	target = chunk_size(data);
-	while (*)
-	while (temp->index <= target)
+	data->new_size = data->max - data->min;
+	while (temp)
 	{
-		
+		if (temp->index >= data->min && temp->index <= data->max)
+			chunk_part(a, b, data)
+		temp = temp->next;
 	}
 }
 
 void	chunk(t_list **a, t_list **b, t_data *data)
 {
+	int	i;
+	int	chunks;
 
+	i = 0;
+	chunks = chunk_count(data);
+	remainder = data->size % chunks;
+	while (i < chunks)
+	{
+		if (i < remainder)
+		{
+			data->min = i * data->chunk_size;
+			data->max = (i + 1) * data->chunk_size + 1;
+		}
+		else
+		{
+			data->min = i * data->chunk_size;
+			data->max = (i + 1) * data->chunk_size;
+		
+		}
+		chunk_sort(a, b, data);
+		i++;
+	}
 }
